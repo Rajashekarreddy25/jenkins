e1a3025c7a1c67e463df48d1e860a3983aa68ea3
Rajashekarreddy25 <vangalarajashekarreddy97@gmail.com>
2026-05-28 04:51:38 +0000

    checking poll method

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 int main(){
 printf("hello jenkins\n");
 printf("gitHub integration with jenkins\n");
-printf("hello from my pc");
+printf("hello from my pc\n");
+printf("check poll method\n");
 return 0;
 }
